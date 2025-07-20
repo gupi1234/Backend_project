1f3bb41a4a9a2e2e7a5ddbafd5561e8921c6935f
gupi debnath <iamgupidebnath@gmail.com>
2025-07-20 21:49:43 +0530

    register controller done

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     // file has been Uploaded successfull
-    console.log("file is uploaded on cloudinary", Response.url);
+    console.log("file is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the locally saved temporary file as  the upload operation got failed..
